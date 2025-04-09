4ed6f3138b2af5e26fcd57c4acdafcbf2d72733b
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-09 15:59:15 +0530

    script.js is updated

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ const growth = document.querySelector(".growth")
 let grow = 0;
 
 btn.addEventListener("click", function(){
-    setInterval(() => {
-        if(grow > 100){
-            
-            clearInterval()
-            btn.innerHTML = "Completed"
-            btn.style.opacity = "0.5"
-            return grow
-        }
-        
+    var int = setInterval(() => {
+        grow++
         percent.innerHTML = grow + "%"
         growth.style.width = grow + "%"
-        grow++
+       
     }, 50)
 
 
-   
+    setTimeout(()=>{
+        clearInterval(int)
+        btn.innerHTML = "Completed"
+        btn.style.opacity = "0.5"
+    },5000)
 })
